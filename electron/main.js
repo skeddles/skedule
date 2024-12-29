c4f1d6d8ca38d57522dc4647910354436d535c1a
Sam Keddy <skeddy13@yahoo.com>
2024-12-29 00:00:29 -0500

    added electron

---
+++ electron/main.js
@@ -0,0 +1,34 @@
+import { app, BrowserWindow, ipcMain } from 'electron';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function createWindow() {
+    let mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js'),
+            contextIsolation: true,
+        },
+    });
+
+    mainWindow.setMenuBarVisibility(false);
+    mainWindow.loadFile(path.join(__dirname, '../dist/index.html'));
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow();
+    }
+});
